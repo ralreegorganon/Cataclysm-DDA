9d5139c689d328223a4f0dbf2d9ecabe735d5de1
Jason Jones <jasonedwardjones@gmail.com>
2018-12-14 15:53:39 -0900

    Add new methods for checking if an omt has been generated
    Add new methods to overmap and overmap buffer to allow checking
    whether or not a given overmap terrain location has been generated,
    which can be used to inform code that wants to change the overmap but
    only wants to do so if the location hasn't been generated already.

--- src/overmap.cpp
+++ src/overmap.cpp
@@ -9,6 +9,7 @@
 #include "line.h"
 #include "map.h"
 #include "map_iterator.h"
+#include "mapbuffer.h"
 #include "mapgen.h"
 #include "mapgen_functions.h"
 #include "messages.h"
@@ -3776,6 +3777,17 @@ std::shared_ptr<npc> overmap::find_npc( const int id ) const
         }
     }
     return nullptr;
+}
+
+bool overmap::is_omt_generated( const tripoint &loc ) const
+{
+    if( !inbounds( loc ) ) {
+        return false;
+    }
+
+    const bool is_generated = MAPBUFFER.lookup_submap( omt_to_sm_copy( loc ) ) != nullptr;
+
+    return is_generated;
 }
 
 overmap_special_id overmap_specials::create_building_from( const string_id<oter_type_t> &base )
