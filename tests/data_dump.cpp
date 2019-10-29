3036e7da7c6b399aeaafe896c34c1be930dc1921
Jason Jones <jasonedwardjones@gmail.com>
2019-10-28 16:25:32 -0800

    wip

--- tests/data_dump.cpp
+++ tests/data_dump.cpp
@@ -35,14 +35,23 @@ TEST_CASE( "overmap_generation_statistics" )
     sqlite3_exec(db, "PRAGMA journal_mode=MEMORY", NULL, NULL, &sErrMsg);
     sqlite3_exec(db, "PRAGMA temp_store=MEMORY", NULL, NULL, &sErrMsg);
     
-    std::string sql = "create table overmap_terrain (overmap_x int, overmap_y int, x int, y int, oter_id text);";
+    std::string sql = "create table overmap_terrain (overmap_x int, overmap_y int, x int, y int, z int, oter_id text);";
     sqlite3_exec(db, sql.c_str(), callback, 0, &sErrMsg);
 
-    std::map<string_id<oter_type_t>, int> occurrences;
-    std::map<ter_id, int> toc;
-    std::map<furn_id, int> foc;
-    std::map<itype_id, int> ioc;
-    std::map<std::pair<string_id<oter_type_t>, itype_id>, int> otit;
+    sql = "create table ot_furniture (overmap_x int, overmap_y int, x int, y int, z int, furn_id text);";
+    sqlite3_exec(db, sql.c_str(), callback, 0, &sErrMsg);
+
+    sql = "create table ot_terrain (overmap_x int, overmap_y int, x int, y int, z int, ter_id text);";
+    sqlite3_exec(db, sql.c_str(), callback, 0, &sErrMsg);
+
+    sql = "create table ot_item (overmap_x int, overmap_y int, x int, y int, z int, itype_id text);";
+    sqlite3_exec(db, sql.c_str(), callback, 0, &sErrMsg);
+
+    // std::map<string_id<oter_type_t>, int> occurrences;
+    // std::map<ter_id, int> toc;
+    // std::map<furn_id, int> foc;
+    // std::map<itype_id, int> ioc;
+    // std::map<std::pair<string_id<oter_type_t>, itype_id>, int> otit;
 
     // Loop through the grid of overmap points
     for( point p : closest_points_first( 0, point_zero ) ) {
@@ -58,9 +67,21 @@ TEST_CASE( "overmap_generation_statistics" )
         
         sqlite3_exec(db, "BEGIN TRANSACTION", NULL, NULL, &sErrMsg);
 
-        std::string isql = "insert into overmap_terrain values (?, ?, ?, ?, ?);";
+        std::string isql = "insert into overmap_terrain values (?, ?, ?, ?, ?, ?);";
         sqlite3_stmt * stmt;
         sqlite3_prepare_v2(db,  isql.c_str(), isql.length(), &stmt, NULL);
+
+        std::string otfsql = "insert into ot_furniture values (?, ?, ?, ?, ?, ?);";
+        sqlite3_stmt * otfstmt;
+        sqlite3_prepare_v2(db,  otfsql.c_str(), otfsql.length(), &otfstmt, NULL);
+
+        std::string ottsql = "insert into ot_terrain values (?, ?, ?, ?, ?, ?);";
+        sqlite3_stmt * ottstmt;
+        sqlite3_prepare_v2(db,  ottsql.c_str(), ottsql.length(), &ottstmt, NULL);
+
+        std::string otisql = "insert into ot_item values (?, ?, ?, ?, ?, ?);";
+        sqlite3_stmt * otistmt;
+        sqlite3_prepare_v2(db,  otisql.c_str(), otisql.length(), &otistmt, NULL);
 
         int z = 0;
         //for( int z = -OVERMAP_DEPTH; z <= OVERMAP_HEIGHT; z++ ) {
@@ -74,30 +95,58 @@ TEST_CASE( "overmap_generation_statistics" )
                     sqlite3_bind_int(stmt, 2, om->global_base_point().y);
                     sqlite3_bind_int(stmt, 3, x);
                     sqlite3_bind_int(stmt, 4, y);
-                    sqlite3_bind_text(stmt, 5, t->get_type_id().c_str(), -1, SQLITE_TRANSIENT);
+                    sqlite3_bind_int(stmt, 5, z);
+                    sqlite3_bind_text(stmt, 6, t->get_type_id().c_str(), -1, SQLITE_TRANSIENT);
 
                     sqlite3_step(stmt);
                     sqlite3_reset(stmt);
-                    // // Run the mapgen for this overmap terrain
-                    // tinymap tmpmap;
-                    // tmpmap.generate( omt_to_sm_copy( tripoint(om->global_base_point(), 0) + tripoint(x, y, z ) ), calendar::turn );
+                    // Run the mapgen for this overmap terrain
+                    tinymap tmpmap;
+                    tmpmap.generate( omt_to_sm_copy( tripoint(om->global_base_point(), 0) + tripoint(x, y, z ) ), calendar::turn );
 
                     // occurrences[t.obj().get_type_id()] += 1;
 
-                    // for(int tx = 0; tx < SEEX*2; tx++) {
-                    //     for(int ty = 0; ty < SEEY *2; ty++) {
-                    //         const ter_id ti = tmpmap.ter({tx, ty});
-                    //         toc[ti] += 1;
-                    //         const furn_id fi = tmpmap.furn({tx, ty});
-                    //         foc[fi] += 1;
-                    //         map_stack ms = tmpmap.i_at({tx, ty, z});
-                            
-                    //         for( item &it : ms ) {
-                    //             ioc[it.typeId()] +=1;
-                    //             otit[{t.obj().get_type_id(), it.typeId()}] +=1; 
-                    //         }
-                    //     }
-                    // }
+                    for(int tx = 0; tx < SEEX*2; tx++) {
+                        for(int ty = 0; ty < SEEY *2; ty++) {
+                            const ter_id ti = tmpmap.ter({tx, ty});
+                            const furn_id fi = tmpmap.furn({tx, ty});
+
+                            sqlite3_bind_int(otfstmt, 1, om->global_base_point().x);
+                            sqlite3_bind_int(otfstmt, 2, om->global_base_point().y);
+                            sqlite3_bind_int(otfstmt, 3, x);
+                            sqlite3_bind_int(otfstmt, 4, y);
+                            sqlite3_bind_int(otfstmt, 5, z);
+                            sqlite3_bind_text(otfstmt, 6, fi->id.c_str(), -1, SQLITE_TRANSIENT);
+
+                            sqlite3_step(otfstmt);
+                            sqlite3_reset(otfstmt);
+
+                            sqlite3_bind_int(ottstmt, 1, om->global_base_point().x);
+                            sqlite3_bind_int(ottstmt, 2, om->global_base_point().y);
+                            sqlite3_bind_int(ottstmt, 3, x);
+                            sqlite3_bind_int(ottstmt, 4, y);
+                            sqlite3_bind_int(ottstmt, 5, z);
+                            sqlite3_bind_text(ottstmt, 6, ti->id.c_str(), -1, SQLITE_TRANSIENT);
+
+                            sqlite3_step(ottstmt);
+                            sqlite3_reset(ottstmt);
+
+
+                            map_stack ms = tmpmap.i_at({tx, ty, z});
+                            for( item &it : ms ) {
+
+                                sqlite3_bind_int(otistmt, 1, om->global_base_point().x);
+                                sqlite3_bind_int(otistmt, 2, om->global_base_point().y);
+                                sqlite3_bind_int(otistmt, 3, x);
+                                sqlite3_bind_int(otistmt, 4, y);
+                                sqlite3_bind_int(otistmt, 5, z);
+                                sqlite3_bind_text(otistmt, 6, it.typeId().c_str(), -1, SQLITE_TRANSIENT);
+
+                                sqlite3_step(otistmt);
+                                sqlite3_reset(otistmt);
+                            }
+                        }
+                    }
                 }
             }
         //}
@@ -129,7 +178,7 @@ TEST_CASE( "overmap_generation_statistics" )
     
 
     std::cout << "END OF TER" << std::endl;
-*/
+
     // for(auto &x : ioc) {
     //     std::cout << x.first << " , " << x.second << std::endl;
     // }
@@ -138,11 +187,13 @@ TEST_CASE( "overmap_generation_statistics" )
         std::cout << x.first.first.str() << " , " << x.first.second << " , " << x.second << std::endl;
     }
 
+
     std::cout << "END OF ITEM" << std::endl;
 
     for(auto &x : occurrences) {
         std::cout << x.first.str() << " , " << x.second << std::endl;
     }
+        */
  
     int count = 0;
     CHECK(count == 10);
